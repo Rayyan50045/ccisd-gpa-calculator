72664e5bf48e4e72b2dbeead23f1d4605b58ad26
unknown <rayyan.usa7@gmail.com>
2026-05-17 11:43:23 -0500

    fix render deployment (gunicorn)

--- app.py
+++ app.py
@@ -64,8 +64,7 @@ def home():
 
     return render_template("index.html", gpa=FinalGPA)
 
-import os
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
